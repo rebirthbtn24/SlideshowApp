6b27d3bf756b30ce674a8ce81ba8a2cf545f9f97
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-17 22:17:05 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 増田弘毅 on 2019/01/17.
+//  Copyright © 2019 rebirthbtn24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
