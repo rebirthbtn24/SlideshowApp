08ab1e6bc423984293fc798398d9a640a7b95f0d
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-21 15:06:27 +0900

    1つ目の画面完成

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,103 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBAction func onPlay(_ sender: Any) {
+        // timer が存在しない場合だけ、タイマーを生成して動作させる
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            playButton.setTitle("停止",for:UIControl.State.normal)
+            prevButton.isEnabled = false
+            nextButton.isEnabled = false
+        }
+        else {
+            self.timer?.invalidate()   // タイマーを停止する
+            self.timer = nil
+            playButton.setTitle("再生",for:UIControl.State.normal)
+            prevButton.isEnabled = true
+            nextButton.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func onPrev(_ sender: Any) {
+        // 表示している画像の番号を1減らす
+        dispImageNo -= 1
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+    }
+    
+    @IBAction func onNext(_ sender: Any) {
+        // 表示している画像の番号を1増やす
+        dispImageNo += 1
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+    }
+    /// 一定の間隔で処理を行うためのタイマー
+    var timer: Timer?
+    
+    // 表示している画像の番号
+    var dispImageNo = 0
+    
+    // 表示している画像の番号を元に画像を表示する
+    func displayImage() {
+        
+        // 画像の名前の配列
+        let imageNameArray = [
+            "pet_cat_omoi_sleep_man.png",
+            "petcart_cat.png",
+            "text_toutoi.png",
+            ]
+        
+        // 画像の番号が正常な範囲を指しているかチェック
+        
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+        
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "pet_cat_omoi_sleep_man.png")
+        // Image Viewに画像を設定
+        imageView.image = image
+    }
+    
+
+    // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
+    @objc func updateTimer(_ timer: Timer) {
+        // 表示している画像の番号を1増やす
+        dispImageNo += 1
+        // 表示している画像の番号を元に画像を表示する
+        displayImage()
+
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
